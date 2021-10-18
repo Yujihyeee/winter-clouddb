22867318b7121072c727a87ca06dd68b212a0b1e
Yujihyeee <rladnddl@naver.com>
2021-10-18 18:42:33 +0900

    back up

--- front-end_old/src/features/counter/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Counter } from 'features/counter/components/Counter'
-// export { default as Counter } from 'features/counter/modules/counterAPI'
-// export { default as Counter } from 'features/counter/modules/counterSlice'
-// export { default as counterSlice.spec } from 'features/counter/modules/counterSlice.spec'
--- front-end_old/src/features/user/index.js
+++
@@ -1,4 +0,0 @@
-export {default as UserJoin} from 'features/user/components/UserJoin'
-export {default as UserList} from 'features/user/components/UserList'
-export {default as SignUp} from 'features/user/containers/SignUp'
-export {default as SignIn} from 'features/user/containers/SignIn'
